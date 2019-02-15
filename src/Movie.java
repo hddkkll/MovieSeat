b2ddc7929011bfba90e9dba38b4d60a50054c8b0
hddkkll <44829588+hddkkll@users.noreply.github.com>
2019-02-15 16:34:15 +0900

    Add files via upload

--- src/Movie.java
+++ src/Movie.java
@@ -1,45 +1,53 @@
 import java.util.Scanner;
 
 public class Movie {
-	private String[] seat = new String[10];
+	private String[][] seat = new String[10][10];
 	private int seatNum = 0;
+	private int seatNum2 = 0;
 	Scanner sc = new Scanner(System.in);
-	
-	public Movie(){
-		for (int i = 0; i < seat.length; i++)
-			seat[i] 	= "ㅁ";
-	}
-	
-	public void showseat() {
-		System.out.println("= = = = = = = = = = = = = = = 자리 = = = = = = = = = = = = = =");
 
+	public Movie() {
+		for (int i = 0; i < seat.length; i++)
+			for(int j=0; j<seat.length;j++)
+				seat[i][j] = "ㅁ";
+	}
+
+	public void showseat() {
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
 		for (int i = 0; i < seat.length; i++) {
-			System.out.print(seat[i] + "\t");
+			for(int j=0; j<seat.length;j++)
+				System.out.print(seat[i][j] + "\t");
+			System.out.println();
 		}
 		System.out.println();
-		
-		select();
 	}
+
 	
-	private void select() {
-		System.out.print("자리 어디로?");
+	public void select() {
+		System.out.print("자리 선택하세요 : ");
+		// do~while -> 자리 입력받고 자리가 범위에 있는지 확인
 		do {
-		seatNum = sc.nextInt();
-		sc.nextLine();
-		if(seatNum>seat.length || seatNum<1)
-			System.out.printf("자리 오류 : 1부터 %d까지만 입력하세요 !\n",seat.length);
+			System.out.println("몇번째줄?");
+			seatNum = sc.nextInt();
+			sc.nextLine();
+			System.out.println("몇번째열?");
+			seatNum2 = sc.nextInt();
+			sc.nextLine();
+
+			if ((seatNum > seat.length || seatNum < 1)&&(seatNum > seat.length || seatNum < 1))
+				System.out.printf("자리 오류 : 1부터 %d까지만 입력하세요 !\n", seat.length);
+
+		} while ((seatNum > seat.length || seatNum < 1) && (seatNum > seat.length || seatNum < 1));
 		
-		
-		}while(seatNum>seat.length || seatNum<1);
-		
-		seat[seatNum - 1] = "*";
-		for (String st : seat)
-			System.out.print(st + "\t");
+		//입력받은 자리가 매진인지 확인
+		if(seat[seatNum-1][seatNum2-1]=="ㅁ") {
+			seat[seatNum-1][seatNum2-1]="[매진]";
+			System.out.println("자리 예매 완료 !");
+		}
+		else {
+			System.out.println("오류 : 매진된 자리입니다 !");			
+		}
+
 		System.out.println();
-		System.out.println("자리 선택 완료 !");
-	}
-	
-	private boolean check(String[] seat, int seatNum) {
-		return true;
 	}
 }
--- src/movie_main.java
+++ src/movie_main.java
@@ -3,37 +3,32 @@ import java.util.Scanner;
 public class movie_main {
 	public static void main(String[] args) {
 		Movie movie = new Movie();
-		Scanner sc = new Scanner(System.in);
-		
-		// int [][] seat = new int[10][10];
-		
+
 		int menuNum = 0;
-		
 
 		do {
 			System.out.println("메뉴 1 : 자리선택");
 			System.out.println("메뉴 2 : 프로그램 종료 !");
-			menuNum = sc.nextInt();
-			sc.nextLine();
+			System.out.println("메뉴 3 : 자리 확인하기");
+			menuNum = movie.sc.nextInt();
+			movie.sc.nextLine();
 
 			switch (menuNum) {
 			case 1:
 				movie.showseat();
+				movie.select();
 				break;
 			case 2:
 				System.out.println("프로그램 종료!");
 				System.exit(0);
 				break;
+			case 3:
+				movie.showseat();
+				break;
 			default:
 				System.out.println("잘못된 숫자 입력");
 			}
 		} while (menuNum != 2);
-
-//		for (int i = 0; i < seat.length; i++) {
-//			seat[i] = "ㅁ";
-//			System.out.print(seat[i] + "\t");
-//		}
-//		System.out.println();
 
 	}
 
